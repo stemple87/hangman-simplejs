0b28f2a10cac7d7f68d4d5f3eadd0befd3d92b2e
Simon Temple <simonctemple@live.com>
2016-03-28 23:39:11 -0700

    think submissions are true and false. trouble in submit area

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,39 @@ var randomizer = function(randomWordArray) {
   return randomWordArray[randomNumber];
 }
 
+var displayCheck = function(guessesLeft) {
+  if (guessesLeft === 6) {
+    $("#hangmanHead").append("0");
+  } else if (guessesLeft === 5) {
+    $("#hangmanLeftArm").append("/");
+  } else if (guessesLeft === 4) {
+    $("#hangmanCenter").append("|");
+  } else if (guessesLeft === 3) {
+    $("#hangmanRightArm").append("\\");
+  } else if (guessesLeft === 2) {
+  $("#hangmanLowerCenter").append("|");
+  } else if (guessesLeft === 1) {
+  $("#hangmanLeftLeg").append("/");
+} else if (guessesLeft === 0) {
+  $("#hangmanRightLeg").append("\\");
+  alert("you lose!");
+  //reset;
+  }
+}
+
 $(document).ready(function(){
   var letterArray = [];
   var guessesLeft = 7;
+  var Correct = true;
+
 
   var randomCondomentWordArray = ["mayonayse", "mustard", "relish", "horseradish", "leuttice", "tomatoes", "catchup", "cheese"];
 
   $("#myButton").click(function(){
     $("#myButton").hide();
-    $("#hangmanHead").append("0");
-    $("#hangmanLeftArm").append("/");
-    $("#hangmanCenter").append("|");
-    $("#hangmanRightArm").append("\\");
-    $("#hangmanLowerCenter").append("|");
-    $("#hangmanLeftLeg").append("/");
-    $("#hangmanRightLeg").append("\\")
+
     var randomWord = randomizer(randomCondomentWordArray);
     var myHangMan = new HangMan(randomWord);
-    var guesses = myHangMan.charLength;
     for(i = 0; i < myHangMan.charLength; i++) {
       letterArray[i] = myHangMan.charArray[i];
       $("#hangmanGuesses").append("<span value='" + myHangMan.charArray[i] + "' id='" + i + "'>_ </span>");
@@ -36,19 +51,20 @@ $(document).ready(function(){
   $(".hangmanForm").submit(function(event){
     event.preventDefault();
     var playerGuess = $("#formInput").val();
-    console.log(playerGuess);
     for (var i = 0; i < letterArray.length; i++) {
       if(letterArray[i] === playerGuess) {
         var idValue = "#" + i;
-        console.log(playerGuess)
-        // $(idValue).empty();
         $(idValue).text(playerGuess);
-        alert("correct")
+        correct = true;
       }
       else {
-        guessesLeft--;
-        console.log(guessesLeft)
+        correct = false;
       }
+    }
+    if (correct === false){
+      guessesLeft--;
+      console.log(guessesLeft);
+      displayCheck(guessesLeft);
     }
   });
 });
